2ae0f77d45bfddc06e379dcbcfc96257f3b6a700
Oscar M. Olivera <oscarmolivera@gmail.com>
2025-03-25 07:02:22 -0400

    [Staging] SolidCache Schema fix

--- db/migrate/20250325010809_add_key_hash_to_solid_cache_entries.rb
+++
@@ -1,6 +0,0 @@
-class AddKeyHashToSolidCacheEntries < ActiveRecord::Migration[8.0]
-  def change
-    add_column :solid_cache_entries, :key_hash, :string, null: false
-    add_index :solid_cache_entries, :key_hash, unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_25_010809) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_24_212106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -34,19 +34,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_25_010809) do
   end
 
   create_table "solid_cache_entries", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "namespace"
-    t.binary "value"
-    t.string "compressor"
-    t.string "coders"
-    t.datetime "expires_at"
-    t.string "version"
+    t.binary "key", null: false
+    t.binary "value", null: false
+    t.bigint "key_hash", null: false
+    t.integer "byte_size", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "key_hash", null: false
-    t.index ["expires_at"], name: "index_solid_cache_entries_on_expires_at"
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key", "key_hash"], name: "index_solid_cache_entries_on_key_and_key_hash", unique: true
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
     t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
-    t.index ["namespace", "key"], name: "index_solid_cache_entries_on_namespace_and_key", unique: true
   end
 
   create_table "tenants", force: :cascade do |t|
