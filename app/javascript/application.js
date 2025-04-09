6628dbc31bcb13256da5879623800631a2c4483a
Oscar M. Olivera <oscarmolivera@gmail.com>
2025-04-09 14:24:17 -0400

    [rbacux] Animate & AOS JS Library add

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,17 @@ import "./vendor/jquery"
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import AOS from "aos";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init({
+    offset: 120, // distance in px before element is revealed
+    delay: 100, // delay in ms before animation starts
+    duration: 800, // duration in ms
+    easing: 'ease-in-out', // AOS built-in easing options
+    once: true, // whether animation should happen only once
+    mirror: false, // whether elements should animate out while scrolling past
+    anchorPlacement: 'top-bottom', // defines trigger point relative to viewport
+  });
+});
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.precompile += %w( favicon.png )
+Rails.application.config.assets.paths << Rails.root.join("app/assets/builds")
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts", "bootstrap-icons")
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images", "vendor")
 
