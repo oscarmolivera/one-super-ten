79f1aed6fa5f466e2e6d6dd577c7f8b2d55d2d40
Oscar M. Olivera <oscarmolivera@gmail.com>
2025-04-09 18:24:04 -0400

    [rbacux] Non animate and Aos library -TO FIX

---
+++ app/javascript/vendor/node-utils.js
@@ -0,0 +1,16 @@
+import "animate.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init({
+    // 👇 YOUR CUSTOM CONFIG
+    offset: 120, // distance in px before element is revealed
+    delay: 100, // delay in ms before animation starts
+    duration: 800, // duration in ms
+    easing: 'ease-in-out', // AOS built-in easing options
+    once: true, // whether animation should happen only once
+    mirror: false, // whether elements should animate out while scrolling past
+    anchorPlacement: 'top-bottom', // defines trigger point relative to viewport
+  });
+});
