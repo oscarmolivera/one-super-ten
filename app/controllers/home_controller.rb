3c124fa7cc2917e355c3eb63489a8905c43c4920
Oscar M. Olivera <oscarmolivera@gmail.com>
2025-04-18 11:38:48 -0400

    [mc2-rbucax] Kamala

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,20 @@
 class HomeController < ApplicationController
-  skip_after_action :verify_policy_scoped, only: [:index, :susudio]
-  skip_before_action :authenticate_user!, only: [:index, :susudio]
+  skip_after_action :verify_policy_scoped, only: [:index, :susudio, :kamala]
+  skip_before_action :authenticate_user!, only: [:index, :susudio, :kamala]
 
   def index; end
 
   def susudio
     authorize :home, :susudio?
+
+    respond_to do |format|
+      format.html
+      format.json { render json: { message: 'ok' } }
+    end
+  end
+
+  def kamala
+    authorize :home, :kamala?
 
     respond_to do |format|
       format.html
