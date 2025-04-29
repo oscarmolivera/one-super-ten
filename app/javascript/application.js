9e82332242c7d6fd1c3d4cd91a2fd36d82b456b7
Oscar M. Olivera <oscarmolivera@gmail.com>
2025-04-29 15:08:37 -0400

    [ml3 beedco] TurboLoad in DOM

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -12,3 +12,5 @@ import PerfectScrollbar from 'perfect-scrollbar'
 window.PerfectScrollbar = PerfectScrollbar
 import "./vendor/template"
 import "./vendor/custom"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = true
