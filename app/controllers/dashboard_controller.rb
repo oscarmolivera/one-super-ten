deed5a5bffba7791c142d18f44de1c220fbb6d91
Oscar M. Olivera <oscarmolivera@gmail.com>
2025-05-19 17:05:06 -0400

    [ml4 nubaco] PlayerRol loads in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,6 @@ class DashboardController < ApplicationController
   def index
     authorize :dashboard, :index?
     @users = policy_scope(User)
+    redirect_to player_player_profile_path(current_user.player) 
   end
 end
