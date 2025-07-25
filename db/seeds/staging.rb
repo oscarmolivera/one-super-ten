0d876c84fbfa1f62f8768d2838c8afac6553393f
Oscar M. Olivera <oscarmolivera@gmail.com>
2025-07-24 20:45:04 -0400

    [staging] Updated Seed File for Staging

--- db/seeds/staging.rb
+++ db/seeds/staging.rb
@@ -11,3 +11,11 @@ load Rails.root.join("db/seeds/users.rb")
 puts "Seeding Roles..."
 load Rails.root.join("db/seeds/roles.rb")
 
+puts "Seeding Players..."
+load Rails.root.join("db/seeds/players.rb")
+
+puts "Seeding Coachs..."
+load Rails.root.join("db/seeds/coachs.rb")
+
+puts "Seeding ExternalPlayers..."
+load Rails.root.join("db/seeds/external_players.rb")
