f8c877724fabd74a0d93ef83769aba9496f07ac7
Oscar M. Olivera <oscarmolivera@gmail.com>
2025-07-25 17:26:04 -0400

    [ml4 nubaco5.2] Update Development Env Seed FIle

--- db/seeds/cups_and_tournaments.rb
+++ db/seeds/cups_and_tournaments.rb
@@ -14,7 +14,7 @@ image_index = [2,3,4,5,6,7,8]
 dice = [true, true, true, false, true, true, true, true, true, true,true, true, true, true, false, true, true, true, true, true]
 category_sample = [10,11,12,13]
 tenant_schools.each do |school|
-  7.times do |i|
+  2.times do |i|
     puts "COPA: #{i}"
     filename ="copa#{image_index.sample}.jpg"
     logo_path = Rails.root.join("app/assets/images/#{filename}")
