0004325dcdfca64c9650b8a939877805733c307f
Oscar M. Olivera <oscarmolivera@gmail.com>
2025-08-04 21:57:09 -0400

    [ml4 nubaco5.2] New MatchPerformance Model Design

--- app/models/match_performance.rb
+++ app/models/match_performance.rb
@@ -1,6 +1,17 @@
 class MatchPerformance < ApplicationRecord
+  acts_as_tenant :tenant
+  
+  belongs_to :tournament
   belongs_to :match, touch: true
-  belongs_to :player, touch: true
+  belongs_to :performer, polymorphic: true # Can be Player or ExternalPlayer
 
   validates :goals, :minutes_played, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  before_validation :set_tournament, on: :create
+
+  private
+
+  def set_tournament
+    self.tournament = match.tournament if match
+  end
 end
