4c9d915398ed2d23b89f8e6b0b4af67f768ff0f7
Oscar M. Olivera <oscarmolivera@gmail.com>
2025-09-09 21:02:27 -0400

    [Staging] FIX Zeitwerk::NameError

--- app/services/season_teams/finish_tournament_service.rb
+++ app/services/season_teams/finish_tournament_service.rb
@@ -1,10 +1,12 @@
-class FinishTournamentService
-  def initialize(season_team)
-    @season_team = season_team
-  end
+module SeasonTeams
+  class FinishTournamentService
+    def initialize(season_team)
+      @season_team = season_team
+    end
 
-  def call
-    @season_team.tournament.update(status: :completed)
-    # Add side effects like emailing or logging
+    def call
+      @season_team.tournament.update(status: :completed)
+      # Add side effects like emailing or logging
+    end
   end
 end
